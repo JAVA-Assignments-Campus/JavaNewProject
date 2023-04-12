615b609961f1b6aad6746598286013bf0e64c337
JehanKandy <jehankandy@gmail.com>
2023-04-12 21:47:38 +0530

    Update

--- src/javanewproject/classes/DB.java
+++ src/javanewproject/classes/DB.java
@@ -9,28 +9,40 @@ import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.DriverManager;  
+import java.sql.SQLException;
 
 /**
  *
  * @author Jehan
  */
 public class DB {
+    /*
     private static String serverName = "localhost";
     private static String userName = "root";
     private static String dbName = "java_project";
     private static int portNumber = 3306;
-    private static String pass = "";
+    private static String pass = "";*/
     
     //make connection
     
-    public static Connection getConnection()
+    public static void getConnection()
     {
-        try {
-            Connection connection = null;
-            
+        String name,pass,url;  
+        Connection con = null;  
+        
+        try {                       
+
             Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection(pass);
-        } catch (ClassNotFoundException ex) {
+            url="jdbc:mysql://localhost:3306/spring"; 
+            name = "root";
+            pass = "";
+            
+            con = DriverManager.getConnection(url,name,pass);  
+            System.out.println("Connection created");  
+            con.close();  
+            System.out.println("Connection closed");
+            
+        } catch (ClassNotFoundException | SQLException ex) {
             Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
